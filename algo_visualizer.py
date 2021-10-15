cd893e4e31305d3e130db987c836fada27134d47
Rajarshi Banerjee <rajarshiban13@gmail.com>
2021-10-16 01:27:10 +0530

    merge sort complete

--- algo_visualizer.py
+++ algo_visualizer.py
@@ -268,17 +268,23 @@ class window:
                     return 
                
                while front<=mid and rj<=last:
+                    self.display(self.N,self.data,['yellow' if x==front or x==rj else 'dodgerblue' for x in range(self.N)])
+                    time.sleep(self.speed)
                     if a[front]<=a[rj]:
+                         self.display(self.N,self.data,['lime' if x==front or x==rj else 'dodgerblue' for x in range(self.N)])
+                         time.sleep(self.speed)
                          front+=1
                     else:
+                         self.display(self.N,self.data,['red' if x==front or x==rj else 'dodgerblue' for x in range(self.N)])
+                         time.sleep(self.speed)
                          temp=a[rj]
                          i=rj
                          while i!=front:
                               a[i]=a[i-1]
-                              self.display(self.N,self.data,['purple' if x==i else 'dodgerblue' for x in range(self.N)])
-                              time.sleep(self.speed)
                               i-=1
                          a[front]=temp
+                         self.display(self.N,self.data,['lime' if x==front or x==rj else 'dodgerblue' for x in range(self.N)])
+                         time.sleep(self.speed)
 
                          front+=1
                          mid+=1
@@ -300,13 +306,14 @@ class window:
           pivot=a[i]
 
           while i<j:
-               while  i<len(a) and a[i]<= pivot :
+               while  i<len(a) and a[i]<= pivot:
                     i+=1
                while a[j]>pivot:
                     j-=1
                if i<j:
                     a[i],a[j]=a[j],a[i]
-               
+               self.display(self.N,self.data,['yellow' if x==i or x==j else "dodgerblue" for x in range(self.N)])
+               time.sleep(self.speed)
           a[j],a[l]=a[l],a[j]
           return j
 
